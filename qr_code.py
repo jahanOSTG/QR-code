4706d54ecedd920284aec89247c0829cbae2b6ab
Faria Jahan Janie <janiefaria315@gmail.com>
2025-02-15 21:41:19 +0600

    Add files via upload

---
+++ qr_code.py
@@ -0,0 +1,14 @@
+import qrcode
+import image
+qr=qrcode.QRCode(
+    version=15,
+    box_size=5,
+    border=5
+)
+
+data="https://github.com/jahanOSTG"
+
+qr.add_data(data)
+qr.make(fit=True)
+img=qr.make_image(fill="black",back_color="white")
+img.save("QR.png")
